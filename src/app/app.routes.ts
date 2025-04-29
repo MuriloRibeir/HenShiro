a24c6b1a03693737dd37bd069c0ce7ab692b6d8d
PedroHenriquMoreira <pedro.moreira80@etec.sp.gov.br>
2025-04-28 22:08:21 -0300

    feat: troca de design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
-import { FirstPageLayoutComponent } from './components/first-page-layout/first-page-layout.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
