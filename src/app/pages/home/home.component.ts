420c7b3160ed38d2f72bf6e2d4233f0dfa8d0914
PedroHenriquMoreira <pedro.moreira80@etec.sp.gov.br>
2025-04-28 22:07:41 -0300

    feat: finalização de cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
   mangas = [
-    { titulo: 'Naruto', imagem: 'assets/naruto.jpg' },
+    { titulo: 'Naruto', imagem: 'images.png' },
     { titulo: 'One Piece', imagem: 'assets/onepiece.jpg' },
     { titulo: 'Bleach', imagem: 'assets/bleach.jpg' },
     { titulo: 'Jujutsu Kaisen', imagem: 'assets/jujutsu.jpg' },
