54d0e7d74dd0a4f4d5af8558aadceea241d703e7
GuilhermAnthony <guilherme.oliveira1110@etec.sp.gov.br>
2025-06-09 21:29:08 -0300

    ajustes basicos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
+        path: "",
+        redirectTo: "login",
+        pathMatch: "full"
+      },
+    {
         path: "login",
         component: LoginComponent
     },
