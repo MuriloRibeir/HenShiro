33e683c5e1073c52f992423ca28065c455e53ca8
GuilhermAnthony <guilherme.oliveira1110@etec.sp.gov.br>
2025-06-17 14:41:59 -0300

    carrossel de recomendações

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [NavbarComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  @ViewChild('mangaGridWrapper') mangaGridWrapper!: ElementRef<HTMLDivElement>;
 
   mangas = [
     { titulo: 'Naruto', imagem: 'naruto-cover.jpeg' },
@@ -18,4 +21,24 @@ export class HomeComponent {
     { titulo: 'Attack on Titan', imagem: 'aot-cover.jpeg' },
     { titulo: 'Death Note', imagem: 'death-note-cover.jpeg' }
   ];
+  
+  scrollAmount: number = 300;
+
+  scrollLeft(): void {
+    if (this.mangaGridWrapper) {
+      this.mangaGridWrapper.nativeElement.scrollBy({
+        left: -this.scrollAmount,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  scrollRight(): void {
+    if (this.mangaGridWrapper) {
+      this.mangaGridWrapper.nativeElement.scrollBy({
+        left: this.scrollAmount,
+        behavior: 'smooth'
+      });
+    }
+  }
 }
